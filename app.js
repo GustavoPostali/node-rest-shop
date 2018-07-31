a8738ef0a736c6f1bfccb8668f6cf9a5ee1c55ca
GustavoPostali <postaligustavo@gmail.com>
2018-07-31 13:06:03 -0300

    Base de dados e Rotas de produtos atualizadas

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://GustavoPostaliAdmin:' + process.env.MONGO_ATLAS_PASSWORD + '@node-rest-shop-shard-00-00-ugxqj.mongodb.net:27017,node-rest-shop-shard-00-01-ugxqj.mongodb.net:27017,node-rest-shop-shard-00-02-ugxqj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser : true });
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded( {extended : false} ));
